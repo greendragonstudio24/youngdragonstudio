cfab0626262c5fc6142f59aa261a833150bbdc43
greendragonstudio24 <greendragonstudio24@gmail.com>
2026-06-03 10:28:12 +0700

    Create products.js

---
+++ products.js
@@ -0,0 +1,26 @@
+const PRODUCTS = [
+    {
+        name: "Color Cube Shooter",
+        image: "images/color-cube-shooter.jpg",
+        description: "Unity puzzle source code with 500+ levels and level editor.",
+        url: "#"
+    },
+    {
+        name: "Gem Sorting Puzzle",
+        image: "images/gem-sorting-puzzle.jpg",
+        description: "Sorting puzzle template with boosters, editor and mobile monetization.",
+        url: "#"
+    },
+    {
+        name: "Kitty Hole Jam",
+        image: "images/kitty-hole-jam.jpg",
+        description: "Casual puzzle game template with 1000+ levels and mobile support.",
+        url: "#"
+    },
+    {
+        name: "Screw Sort Puzzle",
+        image: "images/screw-sort-puzzle.jpg",
+        description: "Unity source code for screw sorting puzzle gameplay.",
+        url: "#"
+    }
+];
